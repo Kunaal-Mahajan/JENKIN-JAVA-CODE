486b91be7062dce8d91bb80fc8f0404e5d89f7a7
Kunaal-Mahajan <kunalmahajan2552@gmail.com>
2026-07-31 01:57:53 +0530

    second commit

--- Main.java
+++ Main.java
@@ -2,7 +2,14 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome");
+        int num = 0;
+        System.out.println("Welcome kunal mahajan");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" " + j);
+            }
+            System.out.println();
+        }
         System.out.println(new Date());
     }
 }
